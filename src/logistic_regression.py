eaffe24732d7b676d8cb1e76bd6237c345b642b4
zishan044 <winchesterfelix007@gmail.com>
2025-08-02 14:32:14 +0600

    refactor for logistic regression code readability

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -7,37 +7,29 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 class LogisticRegression:
-    def __init__(self, alpha=0.001, epochs=10000):
+    def __init__(self, alpha=0.001, epochs=10000, tol=0.00001):
         self.alpha = alpha
         self.epochs = epochs
-        self.weights = None
-        self.bias = None
+        self.tol = tol
+        self.theta = None
 
     def fit(self, X, y):
-        n_samples, n_features = X.shape
+        X_b = np.c_[np.ones(shape=(X.shape[0], 1)), X]
 
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        n_samples, n_features = X_b.shape
+        self.theta = np.zeros(n_features)
 
         for i in range(self.epochs):
-            y_pred = np.dot(X, self.weights) + self.bias
-            pred = sigmoid(y_pred)
-            
-            error = pred - y
+            grad = (X_b.T @ (sigmoid(X_b @ self.theta) - y))/n_samples
+            self.theta -= grad * self.alpha
 
-            dw = (1/n_samples) * np.dot(X.T, error)
-            db = (1/n_samples) * np.sum(error)
-
-            self.weights -= dw * self.alpha
-            self.bias -= db * self.alpha
+            if np.linalg.norm(self.theta) < self.tol:
+                break
+        return self.theta
 
     def predict(self, X):
-        y_pred = np.dot(X, self.weights) + self.bias
-        pred = sigmoid(y_pred)
-        class_pred = (pred > 0.5).astype(int)
-
-        return class_pred
-
+        X_b = np.c_[np.ones(shape=(X.shape[0], 1)), X]
+        return (sigmoid(X_b @ self.theta) >= 0.5).astype(int)
 
 def accuracy(y_test, y_pred):
     return np.sum(y_test == y_pred)/len(y_test)
@@ -45,8 +37,7 @@ def accuracy(y_test, y_pred):
 
 if __name__ == '__main__':
 
-    data = load_breast_cancer()
-    X, y = data.data, data.target
+    X, y = load_breast_cancer(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     scaler = StandardScaler()
